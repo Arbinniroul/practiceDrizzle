f4f9b2b7c4393d32b5caa529854476ac118d566b
Arbinniroul <arbinniroula21@gmail.com>
2025-04-11 23:41:14 +0545

    BackendQueryAdded

--- src/app/sign-up/actions.ts
+++ src/app/sign-up/actions.ts
@@ -3,9 +3,24 @@
 import { redirect } from "next/navigation";
 
 import { wait } from "@/lib/utils";
+import { executeAction } from "@/db/utils/executeAction";
+import { userSchema,user, UserSchema } from "@/db/schema/user";
+import { db } from "@/db";
 
-export async function signUp(data: unknown) {
-	await wait();
-	redirect("/sign-in");
-	return { success: true, message: "Signed up successfully" };
+
+export async function signUp(data: UserSchema) {
+	return executeAction({
+		actionFn:async()=>{
+			const validatedData=userSchema.parse(data);
+			if(validatedData.mode==="signUp"){
+				await db.insert(user).values(validatedData);
+				redirect("/sign-in")
+			}
+			
+		},
+		isProtected:false,
+		clientSuccessMessage:"Signed Up succesfully",
+		serverErrorMessage:'signUp'
+
+	})
 }
